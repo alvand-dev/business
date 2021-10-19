c514d72465138d0a6ace5802dd70581b506ad233
Mohammad177 <m17768@gmail.com>
2021-10-19 16:14:31 +0330

    Add a close icon to the top menu when is active in mobile devices + change it to bar icon when is deactive.

--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -1,17 +1,20 @@
 "use strict";
 
 const topNav = {
-  menu: document.getElementById("top-menu"),
+  menu: document.querySelector("#top-menu"),
   menuLinks: document.querySelectorAll('.top-menu-link'),
+  toggleIcon: document.querySelector('#toggle-icon'),
   toggle: function() {
     if(this.menu.style.display=="block") {
       this.menu.style.display="none";
+      this.toggleIcon.className = 'fas fa-bars';
     } else {
   	  this.menu.style.display="block";
+      this.toggleIcon.className = 'fas fa-times';
     }
   }
 }
 
 topNav.menuLinks.forEach((item) => {
-  item.addEventListener('click', () => { topNav.menu.style.display="none"; });
+  item.addEventListener('click', () => { topNav.toggle(); });
 });
