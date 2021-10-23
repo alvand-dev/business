25b3c689435a24defdab297d13a524616cfa4794
Mohammad177 <m17768@gmail.com>
2021-10-23 15:54:59 +0330

    change gallery section photo numbers in differnt media queries

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,14 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
   new Splide('#gallery', {
     type: 'loop',
     height: '766px',
-    perPage: 4,
+    perPage: 5,
     perMove: 1,
     pagination: false,
     breakpoints: {
+      1199: {
+        perPage: 4,
+      },
       991: {
-        perPage: 2,
+        perPage: 3,
       },
       767: {
+        perPage: 2,
+      },
+      575: {
         perPage: 1,
       }
     },
