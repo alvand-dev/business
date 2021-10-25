0b29c0c2dff6825bcf2bb41af213f850faea17b0
Mohammad177 <m17768@gmail.com>
2021-10-25 15:51:31 +0330

    do some cleaning in back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,18 +1,18 @@
 "use strict";
+
 // When the user scrolls down 700px from the top of the document, show the button
 window.onscroll = function() {backToTop.scroll();};
+
 const backToTop = {
   btn: document.getElementById("back-to-top"), //Get the button
-  // Show and hide the button
-  scroll: function() {
+  scroll: function() {  // Show and hide the button
     if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
       this.btn.style.display = "block";
     } else {
       this.btn.style.display = "none";
     }
   },
-  // When the user clicks on the button, scroll to the top of the document
-  top: function() {
+  top: function() { // When the user clicks on the button, scroll to the top of the document
     window.scrollTo({top: 0, behavior: "smooth"});
   }
 };
