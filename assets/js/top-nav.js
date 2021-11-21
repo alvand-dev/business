d0916a9e1daddff564d7aec96a6e0afc02535763
Mohammad177 <m17768@gmail.com>
2021-11-21 12:18:52 +0330

    rewrite top menu JavaScript and HTML related to and delete * transition.

--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -1,20 +1,27 @@
 "use strict";
 
-const topNav = {
-  menu: document.querySelector("#top-menu"),
-  menuLinks: document.querySelectorAll('.top-menu-link'),
-  toggleIcon: document.querySelector('#toggle-icon'),
-  toggle: function() {
-    if(this.menu.style.display=="block") {
-      this.menu.style.display="none";
-      this.toggleIcon.className = 'fas fa-bars';
-    } else {
-  	  this.menu.style.display="block";
-      this.toggleIcon.className = 'fas fa-times';
-    }
-  }
-};
+{
 
-topNav.menuLinks.forEach((item) => {
-  item.addEventListener('click', () => { topNav.toggle(); });
+// Grab the HTML elements
+const menu = document.querySelector("#top-menu");
+const menuLinks = document.querySelectorAll('.top-menu-link');
+const openMenuBtn = document.querySelector('#open-top-menu');
+const closeMenuBtn = document.querySelector('#close-top-menu');
+
+// Open the top menu
+openMenuBtn.addEventListener('click', () => {
+  menu.classList.add('show-top-menu');
 });
+
+// Open the top menu
+closeMenuBtn.addEventListener('click', () => {
+  menu.classList.remove('show-top-menu');
+});
+
+menuLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('show-top-menu');
+  });
+});
+
+}
