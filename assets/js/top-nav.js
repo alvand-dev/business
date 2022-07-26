15213ca3721627372b47d5c6ecb609ade974b169
alvand1399 <alvand1399@gmail.com>
2022-07-26 17:53:28 +0430

    refactor: Change the way of running the codes
    To fix a JS checker warning and reformat the codes as well

--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -1,25 +1,25 @@
-"use strict";
+(function () {
+  'use strict';
 
-{
   // Grab the HTML elements
-  const menu = document.querySelector("#top-menu");
-  const menuLinks = document.querySelectorAll(".top-menu-link");
-  const openMenuBtn = document.querySelector("#open-top-menu");
-  const closeMenuBtn = document.querySelector("#close-top-menu");
+  const menu = document.querySelector('#top-menu');
+  const menuLinks = document.querySelectorAll('.top-menu-link');
+  const openMenuBtn = document.querySelector('#open-top-menu');
+  const closeMenuBtn = document.querySelector('#close-top-menu');
 
   // Open the top menu
-  openMenuBtn.addEventListener("click", () => {
-    menu.classList.add("show-top-menu");
+  openMenuBtn.addEventListener('click', () => {
+    menu.classList.add('show-top-menu');
   });
 
   // Close the top menu
-  closeMenuBtn.addEventListener("click", () => {
-    menu.classList.remove("show-top-menu");
+  closeMenuBtn.addEventListener('click', () => {
+    menu.classList.remove('show-top-menu');
   });
 
   menuLinks.forEach((item) => {
-    item.addEventListener("click", () => {
-      menu.classList.remove("show-top-menu");
+    item.addEventListener('click', () => {
+      menu.classList.remove('show-top-menu');
     });
   });
-}
+})();
